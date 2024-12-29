155a72cc0fc8ac27f541916c483c0bb83fff1727
shahram <138889794+shahramsamar@users.noreply.github.com>
2024-12-29 22:10:48 +0330

    remove slug

--- website/admin.py
+++ website/admin.py
@@ -5,11 +5,11 @@ from website.models import  Post
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     search_fields = ('title','content',)
-    list_display = ('title','slug','author','status')
-    list_display_links = ('slug','title',)
+    list_display = ('title','author','status')
+    list_display_links = ('title',)
     list_editable =('status',)
-    list_filter = ('slug','status','publish','author')
-    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status','publish','author')
+    # prepopulated_fields = {'slug': ('title',)}
     authcomplete_fields =('author',)
     date_hierarchy = 'created'
     ordering = ('-publish', 'status')
