9d371d8a7eb4f12d4cdae7c8dbf0cf553808a7d4
shahram <138889794+shahramsamar@users.noreply.github.com>
2024-12-29 22:11:08 +0330

    remove slug

--- website/models.py
+++ website/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(verbose_name=_("Title"), max_length=200)
-    slug = models.SlugField(verbose_name=_("Slug"), allow_unicode=True, max_length=200)
+    # slug = models.SlugField(verbose_name=_("Slug"), allow_unicode=True, max_length=200)
     author = models.ForeignKey(User,verbose_name=_("Author"), on_delete=models.SET_NULL, null=True)
     content = models.TextField(_("Content"))
     publish = models.BooleanField(verbose_name=_("Publish"), default=False)
