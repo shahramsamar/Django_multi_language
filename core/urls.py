10ee855eee37ca2e5879a4be9fc31449583d690c
shahram <shahramsamar2010@gmail.com>
2025-07-20 16:30:21 +0330

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -19,7 +19,6 @@ from django.urls import path, include
 
 # translations
 from django.conf.urls.i18n import i18n_patterns
-
 urlpatterns = [path('i18n/', include('django.conf.urls.i18n')),]
 
 # translations
